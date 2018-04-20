bb08f17b84f5af38b8c5762f189473b11bf40c62
Anton Struyk <astruyk@gmail.com>
2018-04-20 11:11:29 -0400

    Stub out the plugin a bit. Add setting for enable/disable.

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: discourse-extra-group-flare-plugin
+# about: Adds extra flare to users based on groups.
+# version: 1.0
+# authors: Anton Struyk
+# url: https://github.com/astruyk/discourse-extra-group-flare-plugin
+
+register_asset "styles.css"
+
+enabled_site_setting :donate_links_enabled
