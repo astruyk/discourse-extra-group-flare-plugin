969a1b09e04274036ebb9f7e56823f0019ac6e24
Anton Struyk <astruyk@gmail.com>
2018-04-20 11:46:03 -0400

    Make a page for group flare settings.

--- plugin.rb
+++ plugin.rb
@@ -7,3 +7,9 @@
 register_asset "styles.css"
 
 enabled_site_setting :group_flare_enabled
+
+add_admin_route 'group_flare_plugin.title', 'group-flare-settings'
+
+Discourse::Application.routes.append do
+	get '/admin/plugins/group-flare-settings' => 'admin/plugins#index', constraints: StaffConstraint.new
+end
